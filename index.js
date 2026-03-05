05b439743ffa2a90f2cd69a32f299280e5b3b766
Tanel Pauls <Tanel.Pauls@voco.ee>
2026-03-05 14:17:17 +0000

    added mongoDB

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ import express from "express";
 import dotenv from "dotenv";
 import stringsController from "./controllers/strings.js";
 import toodeController from "./controllers/products.js";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8013;
+
+mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@127.0.0.1:27017/mongoose_orm`, {
+})
+.then(() => {
+    console.log("MongoDB connected");
+})
+.catch(err => {
+    console.error("MongoDB connection error:", err);
+});
 
 app.use('/', stringsController);
 app.use('/', toodeController);
